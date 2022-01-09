9504c246024a95f091623b915a892d919d851ad1
Erich Woo <f0042bx@m210-linux-26.thayer.dartmouth.edu>
2022-01-09 17:23:51 -0500

    cp 4-8

---
+++ cp8/blinky.c
@@ -0,0 +1,83 @@
+/*
+ * blinky.c -- working with Serial I/O and GPIO
+ *
+ * Assumes the LED's are connected to AXI_GPIO_0, on channel 1
+ *
+ * Terminal Settings:
+ *  -Baud: 115200
+ *  -Data bits: 8
+ *  -Parity: no
+ *  -Stop bits: 1
+ */
+#include <stdio.h>							/* printf(), getchar() */
+#include <stdlib.h>
+#include <strings.h>
+#include "xil_types.h"					/* u32, u16 etc */
+#include "platform.h"						/* ZYBOboard interface */
+#include <xgpio.h>							/* Xilinx GPIO functions */
+#include "xparameters.h"				/* constants used by the hardware */
+#include "led.h"
+
+#define OUTPUT 0x0							/* setting GPIO direction to output */
+#define CHANNEL1 1							/* channel 1 of the GPIO port */
+
+int main() {
+	init_platform();							/* initialize the hardware platform */
+
+	 /* 
+		* set stdin unbuffered, forcing getchar to return immediately when
+		* a character is typed.
+		*/
+	 setvbuf(stdin,NULL,_IONBF,0);
+	 
+	 printf("[Hello]\n");
+	 
+	 // modularized
+	 led_init();
+	 led_set(0, LED_ON);
+
+	 const int len = 64;
+	 char buffer[len];
+
+	 // while input line is not "quit"
+	 while (strcmp(buffer, "q") != 0) {
+		 // start new input line
+		 printf(">");
+
+		 // initialize ptr to end of string/input
+		 int i = 0;
+		 buffer[i] = '\0';
+		 char c;
+		 // while character isn't newline, keep reading
+		 while ( (c = getchar()) != '\r') {
+			 printf("%c", c);  // echo back char
+
+			 // write char to end of buffer
+			 if(i >= len - 1){  // safety check, wrap back to beginning of buffer
+			 	i = 0;
+			 }
+			 buffer[i++] = c;
+			 buffer[i] = '\0';
+		 }
+		 printf("\n");
+
+		 // check if single char && 0 - 3
+		 char* ptr;
+		 long val = strtol(buffer, &ptr, 10);
+		 if (i == 1 && strlen(ptr) == 0 && val >= 0 && val <= 3) {
+			 printf("[%s]\n", buffer);
+			 if (val == 0) {
+				 printf("Toggling led0...\n");
+				 led_toggle(0);
+			 }
+		 }
+	 }
+
+	 printf("---- program exits here ----\n");
+
+	 // turn off led0
+	 led_set(0, LED_OFF);
+
+	 cleanup_platform();					/* cleanup the hardware platform */
+	 return 0;
+}
---
+++ cp8/led.c
@@ -0,0 +1,54 @@
+/*
+ * Erich Woo & Wendell Wu
+ * ENGS 62 Module 1
+ * 9 Jan 2022
+ */
+
+#include "led.h"
+
+static XGpio port;
+
+/*
+ * Initialize the led module
+ */
+void led_init(void) {
+	XGpio_Initialize(&port, XPAR_AXI_GPIO_0_DEVICE_ID);	/* initialize device AXI_GPIO_0 */
+	XGpio_SetDataDirection(&port, CHANNEL1, OUTPUT);	    /* set tristate buffer to output */
+}
+
+/*
+ * Set <led> to one of {LED_ON,LED_OFF,...}
+ *
+ * <led> is either ALL or a number >= 0
+ * Does nothing if <led> is invalid
+ */
+void led_set(u32 led, bool tostate) {
+	u32 mask = XGpio_DiscreteRead(&port, CHANNEL1);
+	if (tostate) mask |= (1 << led);
+	else mask &= ~(1 << led);
+
+	XGpio_DiscreteWrite(&port, CHANNEL1, mask);
+}
+
+/*
+ * Get the status of <led>
+ *
+ * <led> is a number >= 0
+ * returns {LED_ON,LED_OFF,...}; LED_OFF if <led> is invalid
+ */
+bool led_get(u32 led) {
+	u32 bits = XGpio_DiscreteRead(&port, CHANNEL1); // current state of port
+	u32 mask = (0x1 << led); // used to read the specific bit
+	return ((mask & bits) > 0) ? LED_ON : LED_OFF; // if was on, then value would be greater than 0
+}
+
+/*
+ * Toggle <led>
+ *
+ * <led> is a value >= 0
+ * Does nothing if <led> is invalid
+ */
+void led_toggle(u32 led) {
+	XGpio_DiscreteWrite(&port, CHANNEL1, (1 << led) ^ XGpio_DiscreteRead(&port, CHANNEL1));
+}
+
