5bea2753151aa7b2711434eb9dce9824da47f19e
Erich Woo <f0042bx@m210-linux-30.thayer.dartmouth.edu>
2022-02-05 20:06:45 -0500

    cp4 start

---
+++ module4/cp4/ttc.h
@@ -0,0 +1,35 @@
+/*
+ * ttc.h
+ *
+ * NOTE: The TTC hardware must be enabled (Timer 0 on the processing system) before it can be used!!
+ *
+ */
+#pragma once
+
+#include <stdio.h>
+#include "xttcps.h"
+#include "xparameters.h"  	/* constants used by the hardware */
+#include "xil_types.h"		/* types used by xilinx */
+#include "gic.h"
+/*
+ * ttc_init -- initialize the ttc freqency and callback
+ */
+void ttc_init(u32 freq, void (*ttc_callback)(void));
+
+/*
+ * ttc_start -- start the ttc
+ * simultaneously enables ttc interrupts
+ */
+void ttc_start(void);
+
+/*
+ * ttc_stop -- stop the ttc
+ * simultaneously disables ttc interrupts
+ */
+void ttc_stop(void);
+
+/*
+ * ttc_close -- close down the ttc
+ * simultaneously disables ttc interrupts
+ */
+void ttc_close(void);
